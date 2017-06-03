28563d16082f55ec096ea73ee00edc04fc320996
Damien Laughton <dfplaughton@googlemail.com>
2017-06-03 07:42:33 +0100

    Initial Project and Workspace Layout

---
+++ BarFinder/BarFinder/DidFinishLaunchingViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BarFinder
+//
+//  Created by Damien Laughton on 03/06/2017.
+//  Copyright © 2017 Mobilology Limited. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: RootViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ BarFinder/BarFinder/RootViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RootViewController.swift
+//  BarFinder
+//
+//  Created by Damien Laughton on 03/06/2017.
+//  Copyright © 2017 Mobilology Limited. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+  
+}
